269a7790decf0f945a1943d01e8fdf00afea15c5
Liza Kotovich <elizabeth.kotovich@gmail.com>
2022-05-02 11:03:56 +0300

    ADD ALLURE REPORT

--- src/test/java/org/tms/test/BaseTest.java
+++ src/test/java/org/tms/test/BaseTest.java
@@ -1,15 +1,9 @@
 package org.tms.test;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 import org.tms.driver.DriverSingleton;
-import org.tms.page.InventoryPage;
-import org.tms.service.LoginPageService;
-
-public class BaseTest {
+public class BaseTest{
 
     protected WebDriver driver;
 
@@ -17,7 +11,6 @@ public class BaseTest {
     public void setUp() {
         driver = DriverSingleton.getDriver();
     }
-
 
     @AfterTest
     public void stopBrowser() {
